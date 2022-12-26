a0ac0d3b06d32714d8a47c2ca23734554b8a64e9
avessh <86591558+avessh@users.noreply.github.com>
2022-12-26 20:15:34 +0530

    adjusting about section

--- src/components/About.js
+++ src/components/About.js
@@ -19,10 +19,14 @@ export default class About extends Component {
                 <h1 id='content_h1_span'>Web Developer</h1>
               
                <p id='my_info_para'>Hello, I am Electronics and Communication Engineering Undergraduate from Madan Mohan Malaviya University of Technology, Gorakhpur. I have geniune ineterst in web development that how websites build how they work. I build websites using PHP, MySQL , MERN stack. I love to build website templates.</p>
+               
+               <div id='my-para-detail'>
                <h1 className='color my-3'>Email: <span className='detail'> aveshsinghwork@gmail.com</span> </h1>
                <h1 className='color my-3'>Phone:<span  className='detail'> +91-993 541 9845</span></h1>
                <h1 className='color my-3'>Age:<span className='detail'> 20</span></h1>
                <h1 className='color my-3'>Place:<span className='detail' > Gorakhpur, Uttar Pradesh</span></h1>
+               </div>
+              
             </div>
         </div>
      </section>
